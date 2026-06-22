4f7d07390ef909fa65e0ef0b0d22e9218755f68f
Vikas Kharwar <vikaskharwar987@gmail.com>
2026-06-23 02:04:20 +0530

    Added SQLAlchemy user model and session

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,18 @@ from fastapi import FastAPI
 from app.core.config import settings
 from sqlalchemy import text
 from app.db.database import engine
+from app.db.base import Base
+from app.db.database import engine
+from sqlalchemy import inspect
+
+import app.models
 
 app = FastAPI(
     title=settings.APP_NAME,
     version=settings.APP_VERSION
 )
+
+Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def root():
@@ -37,3 +44,10 @@ def db_test():
             "database": "failed",
             "error": str(e)
         }
+    
+@app.get("/tables")
+def get_tables():
+    inspector = inspect(engine)
+    return {
+        "tables": inspector.get_table_names()
+    }
